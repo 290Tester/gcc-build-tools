a3b2c612c92ec1c66bb9fd7e5217f4aa1aa9a93f
Your Name <you@example.com>
2025-08-15 11:04:10 +0800

    ARM64-7

--- scripts/glibc.sh
+++ scripts/glibc.sh
@@ -35,45 +35,4 @@ function build_glibc() {
     call_cmd make install || die "Error while installing glibc"
 
     set_build_state "${FUNCNAME[0]}"
-    # 在之前的 Dockerfile 基础上添加以下内容
-
-# 新增构建参数
-ARG GLIBC_VERSION=2.35
-ARG INSTALL=/opt/${TARGET}-toolchain/${TARGET}
-
-# 复制 glibc 源代码
-COPY glibc-${GLIBC_VERSION}.tar.xz ${SOURCES_DIR}/
-
-# 解压 glibc 源代码
-RUN cd ${SOURCES_DIR} && \
-    tar -xf glibc-${GLIBC_VERSION}.tar.xz
-
-# 设置 glibc 配置
-ENV GLIBC_CONFIGURATION=(
-    "--prefix=${PREFIX}/${TARGET}"
-    "--host=${TARGET}"
-    "--build=$(gcc -dumpmachine)"
-    "--with-headers=${PREFIX}/${TARGET}/include"
-    "--enable-add-ons"
-    "--enable-obsolete-rpc"
-    "--enable-kernel=4.14"
-    "--disable-werror"
-    "--with-__thread"
-    "--with-tls"
-    "--with-fp"
-    "--with-arch=armv8-a"
-    "--with-tune=cortex-a72"
-)
-
-# 在安装完 GCC 工具链后添加 glibc 构建步骤
-RUN /bin/bash -c "source /home/script_dir/gcc-build-tools/build-gcc && \
-    mkdir -p ${INSTALL}/lib && \
-    mkdir -p ${INSTALL}/include && \
-    build_glibc_header"
-
-RUN /bin/bash -c "source /home/script_dir/gcc-build-tools/build-gcc && \
-    build_glibc"
-
-# 验证 glibc 安装
-RUN test -f ${INSTALL}/lib/libc.so.6 || { echo "GLIBC installation verification failed"; exit 1; }
 }
